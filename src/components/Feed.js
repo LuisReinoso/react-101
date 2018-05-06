cee2e646e6b23f2f4fd7c6e27c6f541852a53b89
Luis Reinoso <lomejordejr@riseup.net>
2018-05-05 21:18:19 -0500

    Agregado componente y modelo tweet

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Tweet from './Tweet';
 
 class Feed extends Component {
 
@@ -12,20 +13,54 @@ class Feed extends Component {
     // this.myCustomMethod = this.myCustomMethod.bind(this);
   }
 
+  renderTweet(tweet) {
+    return (
+      <li key={tweet.id}>
+        <Tweet key={tweet.id} tweet={tweet}/>
+      </li>
+    );
+  }
+
   render() {
+    const tweets = this.props.tweets;
+
     return (
       <div className="feed">
         <h2 className="title">
           Feed
         </h2>
         <ul className="media-list tweets">
-          {/* Tweets */}
+          {tweets.data.map(this.renderTweet)}
         </ul>
       </div>
     )
   }
 }
 
-Feed.propTypes = {};
+Feed.propTypes ={
+  tweets: PropTypes.object.isRequired
+};
+
+Feed.defaultProps = (function() {
+  const tweet = {
+    id: 1,
+    cid: 'c1',
+    state: 'RESOLVED',
+    data: {
+      id: 1,
+      user: 1,
+      text: 'Nothing can beat science!',
+      createdAt: '2016-10-04T05:10:49.382Z'
+    }
+  };
+
+  return {
+    tweets: {
+      state: 'RESOLVED',
+      data: [tweet]
+    }
+  }
+})();
+
 
 export default Feed;
---
+++ src/components/Tweet.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+import moment from 'moment';
+
+class Tweet extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Set your initial state here
+    // this.setState = {};
+
+    // Bind your custom methods so you can access the expected 'this'
+    // this.myCustomMethod = this.myCustomMethod.bind(this);
+  }
+
+  render() {
+    const tweet = this.props.tweet;
+    const user = this.props.user;
+    const timestamp = moment(tweet.data.createdAt).fromNow().split(' ago')[0];
+
+    return (
+      <li className="list-group-item tweet">
+        <div className="image-container">
+          <img
+            className="img-circle avatar"
+            src={user.data.avatar} />
+        </div>
+        <div className="content-container">
+          <h4 className="list-group-item-heading title">
+            {user.data.nickname}
+          </h4>
+          <h4 className="list-group-item-heading timestamp">
+            {'- ' + timestamp }
+          </h4>
+          <p className="list-group-item-text text">
+            {tweet.data.text}
+          </p>
+        </div>
+      </li>
+    );
+  }
+}
+
+Tweet.propTypes = {
+  tweet: PropTypes.object.isRequired
+};
+
+Tweet.defaultProps = {
+  user: {
+    id: 1,
+    data: {
+      id: 1,
+      nickname: "lucca",
+      avatar: "https://cloud.githubusercontent.com/assets/2637399/19027072/a36f0c7a-88e1-11e6-931e-7f67fe01367b.png"
+    }
+  }
+};
+
+export default Tweet;
